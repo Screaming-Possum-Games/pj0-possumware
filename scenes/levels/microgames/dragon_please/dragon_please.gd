263c4a904b238ef95d007e9c0a78c76e06da8ad9
Shane B. <shane.bell.dev@gmail.com>
2026-08-08 18:26:02 -0400

    Breaking: Paper teleports to cursor on click.

---
+++ scenes/levels/microgames/dragon_please/dragon_please.gd
@@ -0,0 +1,34 @@
+extends Microgame
+
+enum PaperStampState {
+    UNSTAMPED,
+    APPROVED,
+    DENIED,
+}
+
+var held_object = null
+var paper_state: Dictionary[String, PaperStampState] = {}
+
+
+func _ready():
+    super._ready()
+    timer.timeout.connect(func(): print("Timer finished!"))
+    for node in get_tree().get_nodes_in_group("pickable"):
+        node.clicked.connect(_on_pickable_clicked)
+
+
+func _unhandled_input(event):
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+        if held_object and !event.pressed:
+            held_object.drop(Input.get_last_mouse_velocity())
+            held_object = null
+
+
+func stamp_paper(paper_name: String, stamp_state: PaperStampState):
+    paper_state[paper_name] = stamp_state
+
+
+func _on_pickable_clicked(object):
+    if !held_object:
+        object.pickup()
+        held_object = object
---
+++ scenes/levels/microgames/dragon_please/paper.gd
@@ -0,0 +1,63 @@
+extends RigidBody2D
+
+signal clicked
+
+@export var sprite_node: Sprite2D
+var held = false
+
+
+func _unhandled_input(event):
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+        if event.pressed:
+            print("clicked")
+            clicked.emit(self)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    _create_collision_polygon(sprite_node)
+
+
+func _physics_process(_delta):
+    if held:
+        global_transform.origin = get_global_mouse_position()
+
+
+func pickup():
+    if held:
+        return
+    freeze = true
+    held = true
+
+
+func drop(impulse = Vector2.ZERO):
+    if held:
+        freeze = false
+        apply_central_impulse(impulse)
+        held = false
+
+
+func _create_collision_polygon(sprite: Sprite2D):
+    """
+    Stolen and updated from here: https://github.com/mellowminx/demo-2023/blob/main/project/autopolygonsprite.gd
+    """
+    var bm = BitMap.new()
+    bm.create_from_image_alpha(sprite.texture.get_image())
+    var rect = Rect2(0, 0, sprite.texture.get_width(), sprite.texture.get_height())
+    var my_array = bm.opaque_to_polygons(rect, 2)
+    var my_polygon = Polygon2D.new()
+    my_polygon.set_polygons(my_array)
+    var offsetX = 0
+    var offsetY = 0
+    if (sprite.texture.get_width() % 2 != 0):
+        offsetX = 1
+    if (sprite.texture.get_height() % 2 != 0):
+        offsetY = 1
+    for i in range(my_polygon.polygons.size()):
+        var my_collision = CollisionPolygon2D.new()
+        my_collision.set_polygon(my_polygon.polygons[i])
+        my_collision.position -= Vector2(
+            (sprite.texture.get_width() / 2) + offsetX, (sprite.texture.get_height() / 2) + offsetY
+        ) * scale.x
+        my_collision.scale = scale
+        add_child(my_collision)
