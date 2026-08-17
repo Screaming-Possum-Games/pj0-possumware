52401e354d4dee97b135d0161d09a2f014a64799
ItsStumpy <joshua.knox927@gmail.com>
2026-08-16 20:46:46 -0400

    drag and drop completed with only inventory being draggable. woop woop!

--- scenes/levels/microgames/battle_merge/battle_merge.gd
+++ scenes/levels/microgames/battle_merge/battle_merge.gd
@@ -6,6 +6,7 @@ extends Microgame
 @export var inventory_marker: Array[Marker2D] = []
 
 
+# I am not sure if I really need these arrays, but they're here 😀
 @onready var enemy_runes_container: Node2D = $GameBoard/EnemyRunes
 var enemy_runes: Array[Area2D] = []
 
@@ -14,6 +15,7 @@ var player_runes: Array[Area2D] = []
 
 @onready var inventory_runes_container: Node2D = $Inventory/InventoryRunes
 var inventory_runes: Array[Area2D] = []
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,6 +28,7 @@ func _ready() -> void:
     for marker in inventory_marker:
         spawn_random_inventory(marker)
 
+
 # on game start
 ## spawn 3 enemy runes
 ### get ref and place @ markers 4/5/6
@@ -35,35 +38,40 @@ func _ready() -> void:
 ## spawn inventory of 3 runes
 func spawn_random_enemy(marker: Marker2D) -> void:
     var n := randi_range(1, 36)
-    var image: Image = Image.load_from_file("res://assets/battle_merge/PNG/Black/Slab (outline)/runeBlack_slabOutline_%0*d.png" % [3,n])
-    var texture: ImageTexture = ImageTexture.create_from_image(image)
+
+    var calculated_attack = n + randi_range(1, 5)
+
+    var img_texture_path := "res://assets/battle_merge/PNG/Black/Slab (outline)/runeBlack_slabOutline_%0*d.png" % [3,n]
+    var img_texture: Texture2D = load(img_texture_path)
     var enemy: Rune = load("res://scenes/levels/microgames/battle_merge/rune.tscn").instantiate()
-    enemy.setup_rune(texture, marker.global_position, "EnemyRunes")
+
+    enemy.setup_rune(img_texture, marker.global_position, "EnemyRunes", calculated_attack)
     # adding data
-    #enemy_runes.append(enemy)
+    enemy_runes.append(enemy)
     enemy_runes_container.add_child(enemy)
 
 
 func spawn_player_runes(marker: Marker2D) -> void:
-    var image: Image = Image.load_from_file("res://assets/battle_merge/PNG/Blue/Slab (outline)/runeBlue_slabOutline_001.png")
-    var texture: ImageTexture = ImageTexture.create_from_image(image)
+    var starting_attack = 5
+    var img_texture_path := "res://assets/battle_merge/PNG/Blue/Slab (outline)/runeBlue_slabOutline_001.png"
+    var img_texture: Texture2D = load(img_texture_path)
     var player_rune: Rune = load("res://scenes/levels/microgames/battle_merge/rune.tscn").instantiate()
-    player_rune.setup_rune(texture, marker.global_position, "PlayerRunes")
-    # adding data
+
+    player_rune.setup_rune(img_texture, marker.global_position, "PlayerRunes", starting_attack)
     player_runes.append(player_rune)
     player_runes_container.add_child(player_rune)
-    var new_drop_zone := DropZone.new()
-    player_rune.add_child(new_drop_zone)
 
-    new_drop_zone.drop_behavior = DropBehaviorReplace.new()
+    #var new_drop_zone := DropZone.new()
+    #player_rune.add_child(new_drop_zone)
+
+    #new_drop_zone.drop_behavior = DropBehavior.new()
     # since there is no valid "snap" space due to the collision shape being the exact size of Rune
     # disabling snap prevents Draggable from looking for an empty space and rejecting.
-    new_drop_zone.snap_style = DropZone.SNAP_STYLE.NO_SNAP
+    #new_drop_zone.snap_style = DropZone.SNAP_STYLE.NO_SNAP
     # creating the type that the drop zone will accept via ID
-    var t := DraggableType.new()
-    t.id = "InventoryRune"
-    new_drop_zone.accepted_draggable_types = [t]
-
+    #var t := DraggableType.new()
+    #t.id = "InventoryRune"
+    #new_drop_zone.accepted_draggable_types = [t]
 
 
 # gameplay loop
@@ -74,31 +82,39 @@ func spawn_player_runes(marker: Marker2D) -> void:
 ## win condition: just drag better /shrug
 func spawn_random_inventory(marker: Marker2D) -> void:
     var n := randi_range(1, 36)
-    var image: Image = Image.load_from_file("res://assets/battle_merge/PNG/Blue/Slab (outline)/runeBlue_slabOutline_%0*d.png" % [3,n])
-    var texture: ImageTexture = ImageTexture.create_from_image(image)
+    var inventory_attack = n * 2
+    var img_texture_path := "res://assets/battle_merge/PNG/Blue/Slab (outline)/runeBlue_slabOutline_%0*d.png" % [3,n]
+    var img_texture: Texture2D = load(img_texture_path)
     var inventory_rune: Rune = load("res://scenes/levels/microgames/battle_merge/rune.tscn").instantiate()
-    inventory_rune.setup_rune(texture, marker.global_position, "InventoryRunes")
-    # adding data
+
+    inventory_rune.setup_rune(img_texture, marker.global_position, "InventoryRunes", inventory_attack)
     inventory_runes.append(inventory_rune)
     inventory_runes_container.add_child(inventory_rune)
-    var new_draggable := Draggable.new()
-    inventory_rune.add_child(new_draggable)
+
+    #var new_draggable := Draggable.new()
+    #inventory_rune.add_child(new_draggable)
     # gave the draggable a targetable ID for drop zone
-    new_draggable.type.id = "InventoryRune"
+    #new_draggable.type.id = "InventoryRune"
 
 
-# rune data
-## name
-## atk points
+func remove_inventory_rune_from_array(rune_node: Rune) -> void:
+    if rune_node in inventory_runes:
+        inventory_runes.erase(rune_node)
 
 
+func did_i_win() -> void:
+    var score := 0
+    var enemy_score := 0
 
-# inventory data
-## holds 3 runes at all times
-## after dragging and dropping a rune from the inventory, replace it (might not have time)
+    for rune in player_runes:
+        if is_instance_valid(rune):
+            score += rune.attack_points
 
+    for rune in enemy_runes:
+        if is_instance_valid(rune):
+            enemy_score += rune.attack_points
 
-# board data
-## 3 player runes with stats
-## 3 enemy runes with stats
-## W/L checks?
+    if score >= enemy_score:
+        print("Win!")
+    else:
+        print("Lose...")
--- scenes/levels/microgames/battle_merge/rune.gd
+++ scenes/levels/microgames/battle_merge/rune.gd
@@ -7,16 +7,65 @@ class_name Rune extends Area2D
 # onbodyentered check for le drag -> LET IT GOOOO
 
 
-#func _ready() -> void:
-    #if is_draggable:
-        #var new_draggable = Draggable.new()
-        #add_child(new_draggable)
-    #else:
-        #return
+# rune data
+## name
+## atk points
+@export var attack_points: int = 5
+
+var is_dragging: bool = false
+var drag_offset: Vector2 = Vector2.ZERO
+var starting_position: Vector2 = Vector2.ZERO
 
 
+func setup_rune(texture: Texture2D, target_position: Vector2, group_name: String, setup_attack_points: int):
+    attack_points = setup_attack_points
 
-func setup_rune(texture: Texture2D, target_position: Vector2, group_name: String):
     $Sprite2D.texture = texture
+    $Label.text = str(setup_attack_points)
     position = target_position
     add_to_group(group_name)
+    starting_position = target_position
+
+
+func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
+    if not is_in_group("InventoryRunes"):
+        return
+
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+        if event.pressed:
+            is_dragging = true
+            drag_offset = global_position - get_global_mouse_position()
+            z_index = 10
+
+
+func _input(event: InputEvent) -> void:
+    if is_dragging:
+        if event is InputEventMouseMotion:
+            global_position = get_global_mouse_position() + drag_offset
+        elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+            is_dragging = false
+            z_index = 0
+            _check_for_drop_merge()
+
+
+func _check_for_drop_merge() -> void:
+    var overlapping_areas = get_overlapping_areas()
+    var merged_successfully := false
+
+    for area in overlapping_areas:
+        if area.is_in_group("PlayerRunes") and area is Rune:
+            area.attack_points += self.attack_points
+            area.get_node("Label").text = str(area.attack_points)
+            merged_successfully = true
+            break
+        else:
+            return
+
+    if merged_successfully:
+        var manager = get_tree().current_scene
+        if manager.has_method("remove_inventory_rune_from_array"):
+            manager.remove_inventory_rune_from_array(self)
+        queue_free()
+    else:
+        var tween = create_tween()
+        tween.tween_property(self, "global_position", starting_position, 0.25).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
